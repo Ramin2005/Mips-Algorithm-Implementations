75c4512042df58325f9dab892ba735a81d822404
Ramin <riranmanesh05@gmail.com>
2026-06-13 13:18:30 +0330

    Update BubbleSort.asm

--- Sort/BubbleSort.asm
+++ Sort/BubbleSort.asm
@@ -10,7 +10,7 @@
 BubbleSort:
 
     slti    $t6, $a1, 2
-    bne     $t6, $zero, done        # if array has one element goto done
+    bne     $t6, $zero, end        # if array has one element goto end
 
     addi	$t0, $zero, 0			# $t0 = 0
 
@@ -22,33 +22,33 @@ BubbleSort:
 
 
     loop_1:
-        beq		$t0, $t2, done	    # if $t0 == $t2 then goto done
-        addi	$t0, $t0, 1			# $t0 = $t0 + 1
-        addi	$t1, $a0, 0			# ArrayStartAddress
-        addi	$t7, $zero, 0		# $t7 = 0
+        beq		$t0, $t2, end	        # if $t0 == $t2 then goto end
+        addi	$t0, $t0, 1			    # $t0 = $t0 + 1
+        addi	$t1, $a0, 0			    # ArrayStartAddress
+        addi	$t7, $zero, 0		    # $t7 = 0
     
 
         loop_2:
-            lw		$t4, 0($t1)		# load element i
-            lw		$t5, 4($t1)		# load element i + 1
+            lw		$t4, 0($t1)		        # load element i
+            lw		$t5, 4($t1)		        # load element i + 1
 
-            slt		$t6, $t5, $t4		            # $t6 = ($t5 < $t4) ? 1 : 0
-            beq		$t6, $zero, loop_2_end_part	    # if $t6 == 0 then goto loop_2_end_part
+            slt		$t6, $t5, $t4	        # $t6 = ($t5 < $t4) ? 1 : 0
+            beq		$t6, $zero, loop_2_end	# if $t6 == 0 then goto loop_2_end
         
-            sw		$t5, 0($t1)		# store $5 into memory with address $t1
-            sw		$t4, 4($t1)		# store $4 into memory with address $t1 + 4
-            addi	$t7, $t7, 1		# $t7 = 1
+            sw		$t5, 0($t1)		        # store $5 into memory with address $t1
+            sw		$t4, 4($t1)		        # store $4 into memory with address $t1 + 4
+            addi	$t7, $t7, 1		        # $t7 = 1
             
 
-            loop_2_end_part:
-                addi	$t1, $t1, 4		    # $t1 = $t1 + 4
-                bne		$t1, $t3, loop_2	# if $t1 != $t3 then goto loop_2
+            loop_2_end:
+                addi	$t1, $t1, 4		        # $t1 = $t1 + 4
+                bne		$t1, $t3, loop_2	    # if $t1 != $t3 then goto loop_2
 
 
-        addi     $t3, $t3, -4          # $t3 = $t3 - 4
-        beq		 $t7, $zero, done	   # if array sorted then goto done
-        j loop_1 # goto loop_1
+        addi     $t3, $t3, -4           # $t3 = $t3 - 4
+        beq		 $t7, $zero, end	    # if array sorted then goto end
+        j loop_1                        # goto loop_1
 
 
-    done:
-        jr $ra   # goto reverse address
+    end:
+        jr $ra                          # goto reverse address
