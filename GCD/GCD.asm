1fecd25b1b34b97b17c2e0ea881d3c7a4ae1cdca
Ramin <riranmanesh05@gmail.com>
2026-08-30 18:36:24 +0330

    Update GCD.asm

--- GCD/GCD.asm
+++ GCD/GCD.asm
@@ -1,3 +1,27 @@
+# $t0 -> a
+# $t1 -> b
+# $t2 -> flag & temp
 
 GCD:
-    
+    move 	$t0, $a0		    # $t0 = $a0
+    move 	$t1, $a1		    # $t1 = $a1
+
+    slt		$t2, $t0, $t1	    # $t2 = ($t0 < $t1) ? 1 : 0
+
+    beq     $t2, $zero, loop    # if $t2 == $zero jump to loop
+    move 	$t2, $t0		    # $t2 = $t0
+    move 	$t0, $t1		    # $t0 = $t1
+    move 	$t1, $t2		    # $t1 = $t2
+
+
+    loop:
+        div		$t0, $t1			# $t0 / $t1
+        mfhi	$t2					# $t2 = $t0 % $t1
+        move 	$t0, $t1		    # $t0 = $t1  
+        move 	$t1, $t2		    # $t1 = $t2
+
+        bne		$t1, $zero, loop	# if $t1 != 0 jump to loop
+
+    end:
+        move 	$v0, $t0		    # $v0 = $t0
+        jr $ra                      # jump to reverse address
