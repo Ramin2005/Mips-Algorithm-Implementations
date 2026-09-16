5c51d12e3fb5c5884dedcd9d4242ff2cb0429f89
Ramin <riranmanesh05@gmail.com>
2026-09-16 18:23:01 +0330

    Update MergeSort_2.asm

--- Sort/MergeSort_2.asm
+++ Sort/MergeSort_2.asm
@@ -1,6 +1,15 @@
-
-# $a0 -> Array start address
-# $a1 -> Array last element address
+# $a0 -> Array start address / Heap allocation size / Temp array start address
+# $a1 -> Array last element address / Midpoint address
+# $a2 -> Temp array last element address
+# $a3 -> Original array start address (passed to Merge)
+# $v0 -> Syscall service 9 result (allocated memory address)
+# $t0 -> Base case flag / Source pointer in copy loop / Midpoint offset
+# $t1 -> Copy loop end address condition
+# $t2 -> Destination pointer in copy loop
+# $t3 -> Temp array start address (loaded from stack for Merge)
+# $t4 -> Temp element value during copy / Temp array end address
+# $t7 -> Array size in bytes
+# $ra -> Return address
 
 MergeSort:
     slt		$t0, $a1, $a0		# $t0 = ($a1 < $a0) ? 1 : 0
@@ -79,6 +88,21 @@ MergeSort:
     end:
         jr  $ra                     # jump to return address
 
+
+
+# $a0 -> Temp array start address (input)
+# $a1 -> Temp array midpoint address (input)
+# $a2 -> Temp array last element address (input)
+# $a3 -> Original array start address (input)
+# $s0 -> Temp midpoint address (left half end limit)
+# $s1 -> Temp last element address + 4 (right half end limit)
+# $t0 -> Original array write pointer
+# $t1 -> Left half read pointer
+# $t2 -> Right half read pointer
+# $t4 -> Value loaded from left half / Temp storage
+# $t5 -> Value loaded from right half
+# $t6 -> Comparison result flag $t4 < $t5
+# $ra -> Return address
 
 Merge:
     addi    $sp, $sp, -8        # Allocate space on stack for 2 registers ($s0, $s1)
